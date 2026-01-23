c61f4cf446f4a39c0c38a08852aae5bba39b62cc
Max Ghenis <mghenis@gmail.com>
2026-01-23 16:21:58 -0500

    Bump version to 0.2.0

--- src/opencollective/__init__.py
+++ src/opencollective/__init__.py
@@ -3,5 +3,5 @@
 from opencollective.auth import OAuth2Handler
 from opencollective.client import OpenCollectiveClient
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["OpenCollectiveClient", "OAuth2Handler"]
